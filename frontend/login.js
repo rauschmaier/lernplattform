ace615bd3f9aa18bb7081e80e782a710dd5ff6b2
Your Name <you@example.com>
2024-10-07 22:56:31 +0200

    jwt

--- frontend/login.js
+++ frontend/login.js
@@ -12,28 +12,19 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         },
         body: JSON.stringify({ username: username, password: password })
     })
-     .then(response => {
-        console.log('Response Status:', response.status); // Statuscode in der Konsole ausgeben
-        return response.json().then(data => {
-            console.log('Response Data:', data);
-            return { data, status: response.status }; // Beide Werte zurückgeben
-        });
-    })
-    .then(({ data, status }) => {
-    if (status !== 200) {
-        throw new Error(data.message || 'Anmeldung fehlgeschlagen');
-    }
-    // Erfolgreiche Anmeldung
-    document.getElementById('message').innerText = `Willkommen, ${data.username}!`;
-
-    // Speichern des Benutzernamens im localStorage
-    localStorage.setItem('username', data.username);
-	console.log('Username gespeichert:', localStorage.getItem('username'));
-    // Weiterleitung zur Lernplattform-Seite
-    window.location.href = 'index.html'; // Ändere den Dateinamen entsprechend deiner Struktur
-})
-    .catch(error => {
-        console.error('Error:', error); // Fehler in der Konsole ausgeben
-        document.getElementById('message').innerText = error.message;
-    });
+ .then(response => response.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('jwtToken', data.accessToken);
+                    console.log('Token gespeichert:', localStorage.getItem('jwtToken'));
+                    window.location.href = 'index.html';
+                } else {
+                    document.getElementById('message').innerText = 'Anmeldung fehlgeschlagen';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById('message').innerText = 'Anmeldung fehlgeschlagen';
+            });
+        
 });
